629ebc2d7e0d0d02124c5375ec9ae3c3a7d4839c
bmukum <>
2022-06-04 21:34:36 +0000

    Initial commit

---
+++ classes.py
@@ -0,0 +1,60 @@
+# Hash table function with initial capacity and functions defined
+class HashTable:
+    # Constructor with optional initial capacity parameter.
+    # Assigns all buckets with an empty list.
+    def __init__(self, initial_capacity=40):  # there are 40 packages provided
+        self.table = []
+        for i in range(initial_capacity):
+            self.table.append([])
+
+    # Adding new items to the hash table.
+    def insert(self, key, item):
+        bucket = hash(key) % len(self.table)  # find bucket and bucket array to insert the item
+        bucket_l = self.table[bucket]
+
+        # set the key if it's already in the bucket
+        for key_value in bucket_l:
+            if key_value[0] == key:
+                key_value[1] = item
+                return True
+
+        # append the item to the bucket array if it's not present
+        key_value = [key, item]
+        bucket_l.append(key_value)
+        return True
+
+    # Searches for an item with matching key in the hash table.
+    def search(self, key):
+        bucket = hash(key) % len(self.table)  # locating the bucket
+        bucket_l = self.table[bucket]
+        # search for the key in the bucket list
+        for key_value in bucket_l:
+            if key_value[0] == key:
+                return key_value[1]
+        return None
+
+# create package object for packages
+class Package:
+    def __init__(self, ID, address, city, state, zip, delivery_deadline, weight, status, delivery_time):
+        self.ID = ID
+        self.address = address
+        self.city = city
+        self.state = state
+        self.zip = zip
+        self.delivery_deadline = delivery_deadline
+        self.weight = weight
+        self.status = status
+        self.delivery_time = delivery_time
+
+    def __str__(self):  # overwite print(Movie) otherwise it will print object reference
+        return "%s, %s, %s, %s, %s, %s, %s, %s, %s" % (
+        self.ID, self.address, self.city, self.state, self.zip, self.delivery_deadline, self.weight, self.status, self.delivery_time)
+
+class Truck:
+    def __init__(self, time, address, mileage, packages):
+        self.time = time
+        self.address = address
+        self.mileage = mileage
+        self.packages = packages
+    def __str__(self):  # overwite print(Movie) otherwise it will print object reference
+        return "%s, %s, %s, %s" % (self.time, self.address, self.mileage, self.packages)
---
+++ main.py
@@ -0,0 +1,86 @@
+import csv
+import math
+import datetime
+from operator import itemgetter, attrgetter
+from classes import *
+
+#load the address into an addresses variable using the csv module
+addresses = list(csv.reader(open('addresses.csv', encoding='utf-8-sig')))
+
+#load the distance into a distances variable using the csv module
+distances = list(csv.reader(open("distance_data.csv", encoding='utf-8-sig')))
+#print(distances[addressInd(packageHashTable.search(1).address)][addressInd(truckOne.address)])
+
+# function to load all the data from the package.csv file into the package hash table object
+def loadPackageData(fileName):
+    with open(fileName) as package:
+        packageData = csv.reader(package, delimiter=',')
+        next(packageData)  # skip header
+        for package in packageData:
+            packageID = int(package[0])
+            packageAddress = package[1]
+            packageCity = package[2]
+            packageState = package[3]
+            packageZip = package[4]
+            packageDeliveryDeadline = package[5]
+            packageWeight = package[6]
+            packageStatus = "Loaded"
+            packageDeliveryTime = datetime.timedelta(hours=8)
+            # create a package object from the package class
+            packageInfo = Package(packageID, packageAddress, packageCity, packageState, packageZip,
+                                  packageDeliveryDeadline, packageWeight, packageStatus, packageDeliveryTime)
+            # insert it into the hash table
+            packageHashTable.insert(packageID, packageInfo)
+
+#create function to return the index of each address.
+def addressInd(address):
+    for item in addresses:
+        # print(item, address)
+        if address in item[1]:
+            return int(item[0])
+
+#Create new instance of package hash table from the class, and load objects into it
+packageHashTable = HashTable()
+loadPackageData('package.csv')
+
+#create truck 1 and 2 objects from the truck class
+truckOne = Truck(datetime.timedelta(hours=8), "HUB", 0.0, 0)
+truckTwo = Truck(datetime.timedelta(hours=8), "HUB", 0.0, 0)
+
+# packages that must be delivered on truck 2 plus 12 other packages
+listTruckTwoPackages = [3,18,36,38] + list(range(10,17)) + list(range(19,24))
+truckTwo.packages = [packageHashTable.search(i) for i in listTruckTwoPackages]
+# function to return distance between package 1's address and the hub and vice versa
+#print(distances[addressInd(packageHashTable.search(1).address)][addressInd(truckOne.address)])
+
+# packages that must be delivered on truck 2 plus 12 other packages
+listTruckTwoPackages = [3,18,36,38] + list(range(10,17)) + list(range(19,24))
+truckTwo.packages = [packageHashTable.search(i) for i in listTruckTwoPackages]
+#print(truckTwo.packages)
+
+ignore = listTruckTwoPackages + [6, 25, 28, 29]  #don't include delayed packages or those loaded into truck two
+listTruckOnePackages = [i for i in range(1,35) if i not in ignore]  # list of 16 packages satisfying the above
+
+#define functions to load each truck
+#for truck one:
+def truckOneLoad(listOfPackages):
+    truckPackages = []
+    for packageId in listOfPackages:
+        truckPackages.append(packageHashTable.search(packageId))
+    return truckPackages
+
+# for truck 2
+def truckTwoLoad(listOfPackages):
+    truckPackages = []
+    for packageId in listOfPackages:
+        truckPackages.append(packageHashTable.search(packageId))
+    return truckPackages
+  
+# call load function to load the packages from listTruckOnePackages and sort them by delivery deadline
+truckOne.packages = sorted(truckOneLoad(listTruckOnePackages), key=attrgetter('delivery_deadline'))
+
+truckTwo.packages = sorted(truckTwoLoad(listTruckTwoPackages), key=attrgetter('delivery_deadline'))
+
+
+
+
